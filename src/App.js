c912c7a166345fb2f0f27589545af7a4b3d9bf48
imrancu <mdimran3772@gmail.com>
2022-05-12 10:36:43 +0600

    navbar is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { Route, Routes } from "react-router-dom";
 import './App.css';
+import About from './Pages/About/About';
+import Appointment from "./Pages/Appointment/Appointment";
+import ContactUs from "./Pages/ContactUs/ContactUs";
+import Home from './Pages/Home/Home';
+import Login from "./Pages/Login/Login";
+import NotFound from "./Pages/NotFound/NotFound";
+import Reviews from "./Pages/Reviews/Reviews";
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <button class="btn btn-primary">Button</button>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/appointment" element={<Appointment/>} />
+        <Route path="/reviews" element={<Reviews/>} />
+        <Route path="/contact" element={<ContactUs/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/*" element={<NotFound/>} />
+      </Routes>
     </div>
   );
 }
